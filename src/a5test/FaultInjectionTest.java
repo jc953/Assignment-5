f958e8c26db00d675e204d683f4c2cc855081274
iaj8 <iaj8@cornell.edu>
2014-03-24 22:24:25 -0400

    tests

--- src/a5test/FaultInjectionTest.java
+++ src/a5test/FaultInjectionTest.java
@@ -17,13 +17,14 @@ public class FaultInjectionTest {
 	/**
 	 * Test cases to make sure mutations do not return same String.
 	 */
+	@Test
 	public void testA(){
 		ParserImpl p = new ParserImpl();
-		Reader r = new StringReader("3 + 3 * 5 AND 3<4 OR 34 > 7 --> mem[6] := 12 + 7 forward");
+		Reader r = new StringReader("3 + 3 < 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
 		StringBuffer sb = new StringBuffer();
 		Program prog = p.parse(r);
 		for (int i = 0; i < 5; i++){
-			prog.getRandomNode().mutate();
+			Mutation.mutate(prog);
 		}
 		prog.prettyPrint(sb);
 		assertTrue(sb.toString()!="3 + 3 * 5 AND 3<4 OR 34 > 7 --> mem[6] := 12 + 7 forward");
@@ -32,7 +33,7 @@ public class FaultInjectionTest {
 		sb = new StringBuffer();
 		prog = p.parse(r);
 		for (int i = 0; i < 5; i++){
-			prog.getRandomNode().mutate();
+			Mutation.mutate(prog);
 		}
 		prog.prettyPrint(sb);;
 		assertTrue(p.parse(r).toString()!="3 + 3 * 5 OR 34 > 7 --> mem[6] := 12 + 7 wait");
--- src/a5test/ParserTest.java
+++ src/a5test/ParserTest.java
@@ -13,45 +13,42 @@ public class ParserTest {
 	@Test
 	public void testA(){
 		ParserImpl p = new ParserImpl(); 
-		Reader r = new StringReader("3 < 5 --> forward");
+		Reader r = new StringReader("3 < 5 --> forward;");
 		StringBuffer sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(sb.toString().equals("3 < 5 --> forward ;"));
+		assertTrue(sb.toString().equals("3 < 5 --> forward;"));
 		
-		r = new StringReader("3 * 3 > 8 + 9 --> tag [ 3 ]");
+		r = new StringReader("3 * 3 > 8 + 9 --> tag [ 3 ];");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		//System.out.println(sb);
-		assertTrue(sb.toString().equals("3 * 3 > 8 + 9 --> tag [ 3 ] ;"));
+		assertTrue(sb.toString().equals("(3 * 3) > (8 + 9) --> tag[3];"));
 		
-		r = new StringReader("3 + 3 * 5 or 34 > 7 --> mem[6] := 12 + 7 wait");
+		r = new StringReader("3 + 3 < 5 or 34 > 7 --> mem[6] := 12 + 7 wait;");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 or 34 > 7 --> mem[6] := 12 + 7 wait");
+		System.out.println(sb.toString());
+		System.out.println("{(3 + 3) < 5 or 34 > 7} --> mem[6] := (12 + 7) \n                            wait;");
+		assertTrue(sb.toString().equals("{(3 + 3) < 5 or 34 > 7} --> mem[6] := (12 + 7) \n                            wait;"));
 		
-		r = new StringReader("3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
+		r = new StringReader("(3 + 3 * 5) < 4 and (9) > random[4+5] --> bud;");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 and 3 < 4 or 34 > 7 --> mem[6] := 12 + 7 forward");
+		assertTrue(sb.toString().equals("{(3 + (3 * 5)) < 4 and 9 > random[(4 + 5)]} --> bud;"));
 		
-		r = new StringReader("(3 + 3 * 5) < 4 and (9) > random[4+5] --> bud");
+		r = new StringReader("3 + 3 < 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward;");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="(3 + 3 * 5) < 4 and (9) > random[4+5] --> bud");
+		//assertTrue(sb.toString().equals(""));
 		
-		r = new StringReader("3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
+		r = new StringReader("3 < 4 --> forward ; 3 > 4 --> backward;");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
+		//assertTrue(sb.toString().equals("3 < 4 --> forward ; /n 3 > 4 --> backward;"));
 		
-		r = new StringReader("3 < 4 --> forward ; 3 < 4 --> backward");
+		r = new StringReader("{3<4} or {5>8} --> bud;");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 < 4 --> forward ; 3 < 4 --> backward");
-		
-		r = new StringReader("{3<4} < 5 --> bud");
-		sb = new StringBuffer();
-		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="{3<4} < 5 --> bud");
+		System.out.println(sb);
+		assertTrue(sb.toString().equals("{3 < 4 or 5 > 8} --> bud;"));
 	}
 }
