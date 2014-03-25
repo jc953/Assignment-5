69c769393c0706986b75f0e66d0a822bc7768cd7
iaj8 <iaj8@cornell.edu>
2014-03-24 20:45:53 -0400

    1

--- src/a4test/ParserTest.java
+++ src/a4test/ParserTest.java
@@ -2,7 +2,6 @@ package a4test;
 import a4.*;
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.*;
 
@@ -10,37 +9,39 @@ public class ParserTest {
 	/**
 	 * Test cases for Expressions
 	 */
+	@Test
 	public void testA(){
 		ParserImpl p = new ParserImpl(); 
 		Reader r = new StringReader("3 < 5 --> forward");
 		StringBuffer sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(sb.equals("3 < 5 --> forward"));
+		System.out.println(sb);
+		assertTrue(sb.equals("3 < 5 --> forward ;");
 		
 		r = new StringReader("3 * 3 > 8 + 9 --> tag [ 3 ]");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
 		assertTrue(p.parse(r).toString()=="3 * 3 > 8 + 9 --> tag [ 3 ]");
 		
-		r = new StringReader("3 + 3 * 5 OR 34 > 7 --> mem[6] := 12 + 7 wait");
+		r = new StringReader("3 + 3 * 5 or 34 > 7 --> mem[6] := 12 + 7 wait");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 OR 34 > 7 --> mem[6] := 12 + 7 wait");
+		assertTrue(p.parse(r).toString()=="3 + 3 * 5 or 34 > 7 --> mem[6] := 12 + 7 wait");
 		
-		r = new StringReader("3 + 3 * 5 OR 34 > 7 --> mem[6] := 12 + 7 wait");
+		r = new StringReader("3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 AND 3<4 OR 34 > 7 --> mem[6] := 12 + 7 forward");
+		assertTrue(p.parse(r).toString()=="3 + 3 * 5 and 3 < 4 or 34 > 7 --> mem[6] := 12 + 7 forward");
 		
-		r = new StringReader("3 + 3 * 5 OR 34 > 7 --> mem[6] := 12 + 7 wait");
+		r = new StringReader("(3 + 3 * 5) < 4 and (9) > random[4+5] --> bud");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="(3 + 3 * 5) < 4 AND (9) > random[4+5] --> bud");
+		assertTrue(p.parse(r).toString()=="(3 + 3 * 5) < 4 and (9) > random[4+5] --> bud");
 		
-		r = new StringReader("3 + 3 * 5 AND 3<4 OR 34 > 7 --> mem[6] := 12 + 7 forward");
+		r = new StringReader("3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
 		sb = new StringBuffer();
 		p.parse(r).prettyPrint(sb);
-		assertTrue(p.parse(r).toString()=="3 + 3 * 5 AND 3<4 OR 34 > 7 --> mem[6] := 12 + 7 forward");
+		assertTrue(p.parse(r).toString()=="3 + 3 * 5 and 3<4 or 34 > 7 --> mem[6] := 12 + 7 forward");
 		
 		r = new StringReader("3 < 4 --> forward ; 3 < 4 --> backward");
 		sb = new StringBuffer();
