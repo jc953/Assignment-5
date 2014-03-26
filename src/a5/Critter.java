b1f451b86d12b2a00c9d2da79fbb82608d35780c
iaj8 <iaj8@cornell.edu>
2014-03-25 21:43:13 -0400

    critter 3

--- src/a5/Critter.java
+++ src/a5/Critter.java
@@ -1,4 +1,6 @@
 package a5;
+import java.io.*;
+import parse.*;
 import ast.*;
 
 public class Critter {
@@ -10,21 +12,21 @@ public class Critter {
 	int row;
 	int nextColumn;
 	int nextRow;
+	int previousColumn;
+	int previousRow;
 	
 	public Critter(String file, int direction, int column, int row, CritterWorld critterworld){
-		//give rules to the critter 
-		//parseFile();
-		this.direction = direction;
-		this.critterworld = critterworld;
-		this.column = column;
-		this.row = row;
-		switch(direction){
-			case 0: nextColumn = column; nextRow = row +1;
-			case 1: nextColumn = column; nextRow = row +1;
-			case 2: nextColumn = column; nextRow = row +1;
-			case 3: nextColumn = column; nextRow = row +1;
-			case 4: nextColumn = column; nextRow = row +1;
-			case 5: nextColumn = column; nextRow = row +1;
+		try {
+			BufferedReader br =  new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			ParserImpl p = new ParserImpl();
+			program = p.parse(br);
+			this.direction = direction;
+			this.critterworld = critterworld;
+			setPosition(column,row);
+			
+		
+		} catch (FileNotFoundException e) {
+			System.out.println("Error");
 		}
 	}
 	
@@ -36,15 +38,76 @@ public class Critter {
 		mem[4] = mem[4] + Constants.SOLAR_FLUX;
 	}
 	
-	public void move(){
-		//move
-		//take energy
+	public void setPosition(int column, int row){
+		this.column = column;
+		this.row = row;
+		if(column%2==0){
+			switch(direction){
+				case 0: nextColumn = column; nextRow = row+1; 
+					previousColumn = column-1; previousRow = row; break;
+				case 1: nextColumn = column+1; nextRow = row;
+					previousColumn = column-1; previousRow = row-1; break;
+				case 2:	nextColumn = column+1; nextRow = row-1;
+					previousColumn = column-1; previousRow = row; break;
+				case 3: nextColumn = column; nextRow = row-1; 
+					previousColumn = column; previousRow = row+1; break;
+				case 4: nextColumn = column-1; nextRow = row-1;
+					previousColumn = column+1; previousRow = row; break;
+				case 5: nextColumn = column-1; nextRow = row;
+					previousColumn = column+1; previousRow = row-1; break;
+			}
+			
+		}
+		else{
+			switch(direction){
+				case 0: nextColumn = column; nextRow = row+1;
+					previousColumn = column-1; previousRow = row+1; break;
+				case 1: nextColumn = column+1; nextRow = row+1;
+					previousColumn = column-1; previousRow = row; break;
+				case 2:	nextColumn = column+1; nextRow = row;
+					previousColumn = column-1; previousRow = row+1; break;
+				case 3: nextColumn = column; nextRow = row-1;
+					previousColumn = column; previousRow = row+1; break;
+				case 4: nextColumn = column-1; nextRow = row;
+					previousColumn = column+1; previousRow = row+1; break;
+				case 5: nextColumn = column-1; nextRow = row+1;
+					previousColumn = column+1; previousRow = row; break;
+			}
+		}
+	}
+	
+	public boolean move(int forOrBack){
+		assert (forOrBack==-1 || forOrBack==1);
+		if(Constants.MOVE_COST>=mem[4]){
+			return false;
+		}
+		mem[4] -= Constants.MOVE_COST;
+		if (forOrBack==-1){
+			if(!critterworld.hexes[previousColumn][previousRow].isFree()){
+				return false;
+			}
+			else {
+				setPosition(previousColumn, previousRow);
+				critterworld.hexes[previousColumn][previousRow].setCritter(this);
+				return true;
+			}
+		}
+		else{
+			if(!critterworld.hexes[nextColumn][nextRow].isFree()){
+				return false;
+			}
+			else {
+				setPosition(nextColumn, nextRow);
+				critterworld.hexes[previousColumn][previousRow].setCritter(this);
+				return true;
+			}
+		}
 	}
 	
 	public void turn(int n){
 		assert (n == 1 || n == -1);
 		direction = direction + n;
-		//change nextColumn and nextRow
+		setPosition(column,row);
 	}
 	
 	public void eat(){
@@ -106,14 +169,14 @@ public class Critter {
 	}
 	
 	public boolean bud(){
-		//figure out the coordinates of the hex behind
-		if (Constants.BUD_COST>=mem[4]){ //|| hex behind.isFree()){
+		if (Constants.BUD_COST>=mem[4]||!critterworld.hexes[previousColumn][previousRow].isFree()){
 			return false;
 		}
 		else{
 			mem[4] -= Constants.BUD_COST;
-			//initialize a new critter in the space 
-			//behind. Make sure to add it to critterworld.hexes
+			Program temp = new Program(program.);
+			Mutation.mutate(temp);
+			critterworld.hexes[previousColumn][previousRow].setCritter(new Critter());
 			return true;
 		}
 	}
