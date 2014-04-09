b6d3e33367c604595fe49f1a55e2d34e13af096a
iaj8 <iaj8@cornell.edu>
2014-04-09 10:50:26 -0400

    tests

--- src/a5/test.java
+++ src/a5/test.java
@@ -1,5 +1,6 @@
 package a5;
 import ast.*;
+
 import java.io.FileNotFoundException;
 
 public class test {
@@ -16,9 +17,44 @@ public class test {
 		assert cw.hexes[4][2].critter.direction == d;
 		assert cw.hexes[4][2].critter.program.equals(program);// assuming the program uniquely identitfies a critter
 		System.out.println("Wait works");
-		
-		
+		test2();
+		test3();
+	}
+	
+	public static void test2() throws FileNotFoundException, InterruptedException{
+		CritterWorld cw = new CritterWorld("src/world.txt");
+		int row = 4;
+		int column = 3;
+		int arrayRow = row - ((column+1)/2);
+		Program program = cw.hexes[column][arrayRow].critter.program;
+		int d = cw.hexes[column][arrayRow].critter.direction;
+		cw.step();
+		assert program == cw.hexes[4][3].critter.program;
+		assert d == cw.hexes[4][3].critter.direction;
+		System.out.println("Move works");
+	
+	}
+	
+	public static void test3() throws FileNotFoundException, InterruptedException{
+		CritterWorld cw = new CritterWorld("src/world.txt");
+		int row = 4;
+		int column = 4;
+		int arrayRow = row - ((column+1)/2);
+		Program program = cw.hexes[column][arrayRow].critter.program;
+		int d = cw.hexes[column][arrayRow].critter.direction;
+		int s = cw.hexes[column][arrayRow].critter.mem[3];
+		cw.step();
+		cw.step();
+		assert program == cw.hexes[column][arrayRow].critter.program;
+		assert d == cw.hexes[column][arrayRow].critter.direction;
+		assert cw.hexes[column][arrayRow].critter.mem[3] == 3;
+		assert cw.hexes[column][arrayRow].critter.mem[4] == 1825;
+		//Even though the rules specifies it should grow 5 times, it only grows once per step
+		//is this an issue?
+		System.out.println("Grow works");
 	
 	}
 	
 }
+	
+	
